ee2e9f976a6bf154ad27ea621b18cf5192990d3f
togetup <togetup@yeah.net>
2018-07-07 07:50:47 +0800

    消息过滤插件完善：通过后台系统管理系统属性来动态调整要过滤的内容

--- src/plugins/messageFilter/src/java/com/msun/plugin/MessageFilterPlugin.java
+++ src/plugins/messageFilter/src/java/com/msun/plugin/MessageFilterPlugin.java
@@ -1,6 +1,7 @@
 package com.msun.plugin;
 
 import java.io.File;
+import java.util.Map;
 
 import org.jivesoftware.openfire.container.Plugin;
 import org.jivesoftware.openfire.container.PluginManager;
@@ -8,6 +9,9 @@ import org.jivesoftware.openfire.interceptor.InterceptorManager;
 import org.jivesoftware.openfire.interceptor.PacketInterceptor;
 import org.jivesoftware.openfire.interceptor.PacketRejectedException;
 import org.jivesoftware.openfire.session.Session;
+import org.jivesoftware.util.JiveGlobals;
+import org.jivesoftware.util.PropertyEventDispatcher;
+import org.jivesoftware.util.PropertyEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xmpp.packet.Message;
@@ -17,16 +21,30 @@ import org.xmpp.packet.Packet;
  * 插件的初始化工作
  * @author togetup
  */
-public class MessageFilterPlugin implements Plugin, PacketInterceptor {
+public class MessageFilterPlugin implements Plugin, PacketInterceptor, PropertyEventListener {
 	
 	private static final Logger log = LoggerFactory.getLogger(MessageFilterPlugin.class);
 	
 	
 	private InterceptorManager interceptorManager;
-			
+	
+	// 被过滤的内容
+	private String filterContent;
+	
+	/**
+	 * 构造函数
+	 */
+	public MessageFilterPlugin() {
+		// 第一个参数表示数据库表中的属性名，第二个属性表示取不到时的默认值
+		filterContent = JiveGlobals.getProperty("plugin.messagefilter.content", "fuck");
+	}
+
 	@Override
 	public void initializePlugin(PluginManager manager, File pluginDirectory) {
 		log.info("MessageFilter init");
+		
+		// 将此类加入属性监听管理
+		PropertyEventDispatcher.addListener(this);
 		
 		interceptorManager = InterceptorManager.getInstance();
 		interceptorManager.addInterceptor(this);
@@ -48,13 +66,42 @@ public class MessageFilterPlugin implements Plugin, PacketInterceptor {
 				Message msg = (Message)packet;
 				String body = msg.getBody();
 				
-				if (body!=null && body.contains("fuck")) {
+				if (body!=null && body.contains(filterContent)) {
 					PacketRejectedException rejectException = new PacketRejectedException();
-					rejectException.setRejectionMessage("fuck is error");
+					rejectException.setRejectionMessage(filterContent + " is error");
 					throw rejectException;
 				}				
 			}		
 		}		
 		
+	}
+
+	@Override
+	public void propertySet(String property, Map<String, Object> params) {
+		log.info("propertySet:" + property);	
+		
+		// 数据库中的值修改后自动更新程序中的内容
+		if (property.equals("plugin.messagefilter.content")) {
+			filterContent = params.get("value").toString();
+		}
+	}
+
+	@Override
+	public void propertyDeleted(String property, Map<String, Object> params) {
+		log.info("propertyDeleted:" + property);	
+		
+		if (property.equals("plugin.messagefilter.content")) {
+			filterContent = "";
+		}
+	}
+
+	@Override
+	public void xmlPropertySet(String property, Map<String, Object> params) {
+		log.info("xmlPropertySet:" + property);			
+	}
+
+	@Override
+	public void xmlPropertyDeleted(String property, Map<String, Object> params) {
+		log.info("xmlPropertyDeleted:" + property);			
 	}	
 }
